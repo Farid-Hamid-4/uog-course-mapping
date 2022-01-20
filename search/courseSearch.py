c27ec624a8289fc86af0ffc51641f389fb43bd20
htopiwal <htopiwal@uoguelph.ca>
2022-01-19 23:29:08 -0500

    SETUP: Added template code for Python program, moved requirements.txt

--- search/courseSearch.py
+++ search/courseSearch.py
@@ -1,1 +1,34 @@
-print("hello world!")
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Imports
+import argparse
+
+# Program Information
+__author__ = "Harsh Topiwala, Jainil Patel, Andrew Heft, Nicholas Baker, Lourenco Velez, Farid Hamid"
+__version__ = "1.0.0"
+__maintainer__ = "Harsh Topiwala"
+__email__ = "htopiwal@uoguelph.ca"
+__status__ = "Development"
+
+
+"""
+A CLI-based program with the purpose of searching through UoG's courses.
+Last Updated: 1/19/2022, by Harsh Topiwala
+"""
+
+
+def parseCLIArgs():
+    
+def main():
+    """
+    Main Function.
+    :param p1: N/A
+    :return: N/A
+    """ 
+
+    print("hello dangus!")
+
+
+if __name__ == "__main__":
+    main()
