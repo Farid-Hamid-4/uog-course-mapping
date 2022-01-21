25ff3641349cef40ac33820f18eb282b0e116c3f
htopiwal <htopiwal@uoguelph.ca>
2022-01-20 21:25:47 -0500

    CL ARGS: Added functionality to parse CLI input

--- search/courseSearch.py
+++ search/courseSearch.py
@@ -17,9 +17,59 @@ A CLI-based program with the purpose of searching through UoG's courses.
 Last Updated: 1/19/2022, by Harsh Topiwala
 """
 
-
 def parseCLIArgs():
+    """
+    Runs a specific type of search depending on flag set in the command.
+    :return: (String) Result to be displayed back to user.
+    """ 
+
+    # Parser initialization
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(help='Search Methods')
+
+    # Case 1: list all programs
+    #   courseSearch lp
+    listProgramsParser = subparsers.add_parser('lp')
+    listProgramsParser.set_defaults(which='lp')
+
     
+    # Case 2: search by course code: 
+    #   courseSearch cc {Course code, i.e: ACCT*1000}
+    ccParser = subparsers.add_parser('cc', help='Search by course code')
+    ccParser.add_argument('[Course Code]')
+    ccParser.set_defaults(which='cc')
+
+    # Case 3: search by program code: 
+    #   courseSearch pc {Program code}
+    pcParser = subparsers.add_parser('pc', help='Search by program code')
+    pcParser.add_argument('[Program Code]')
+    pcParser.set_defaults(which='pc')
+
+
+    # Case 4: search by credit weight and season: 
+    #   courseSearch cw {0.25, 0.5, 0.75, ... } {Season: optional, default all seasons}
+    cwParser = subparsers.add_parser('cw', help='Search by course weight and season (optional)')
+    cwParser.add_argument('[Course Weight]', choices=['0.00', '0.25', '0.50', '1.00'])
+    cwParser.add_argument('[Offering Season]', type=str.upper, nargs='?', default='all', choices=['S', 'W', 'F', 'ALL'])
+    cwParser.set_defaults(which='cw')
+
+    args = vars(parser.parse_args())
+
+    # Call search functions based on command type    
+    if args['which'] == 'cc':
+        print("Course Code Search")
+
+    elif args['which'] == 'pc':
+        print("Program Code Search")
+
+
+    elif args['which'] == 'cw':
+        print("Course Weight Search")
+
+    else:
+        print("List all programs")
+
+
 def main():
     """
     Main Function.
@@ -27,8 +77,7 @@ def main():
     :return: N/A
     """ 
 
-    print("hello dangus!")
-
+    parseCLIArgs();
 
 if __name__ == "__main__":
     main()
