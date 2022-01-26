f97cee8257100fd00ae2f32260bedd8ddf49ad79
Jainil Patel <jainil@uoguelph.ca>
2022-01-26 12:27:20 -0500

    FIX: renamed variables for better readability

--- scraper/index.js
+++ scraper/index.js
@@ -4,50 +4,50 @@ const fs = require('fs');
 
 /**
  * Program Information
- * @author Harsh Topwala, Jainil Patel, Nicholas Baker, Lourenco Velez, Farid Hamid
+ * @author Farid Hamid, Harsh Topwala, Jainil Patel, Lourenco Velez, Nicholas Baker
  * @version "1.0.0"
- * @maintainer "Nicholas Baker, Jainil Patel" 
- * @email "nbaker05@uoguelph.ca, jainil@uoguelph.ca" 
+ * @maintainer "Jainil Patel, Nicholas Baker" 
+ * @email "jainil@uoguelph.ca, nbaker05@uoguelph.ca" 
  * @status "Development"
  */
 
 /* ----------------- Arrays to store courses for each semester ----------------- */
-let wWin = [];
-let fFal = [];
-let sSum = [];
+let winterCoursesArr = [];
+let fallCoursesArr = [];
+let summerCoursesArr = [];
 
 /**
  * @name getTextFrom
- * @description This fucntion will split the spt value in the str string
- * @param {string} str This string holds the information we want
- * @param {string} spt This string holds what we want to take away from str
+ * @description This fucntion will split the keyword in searchString
+ * @param {string} searchString This string holds the information we want
+ * @param {string} keyword This string holds what we want to take away from searchString
  * @returns {string} With only information we want
  */
-let getTextFrom = (str, spt) => {
-    ret = str.split(spt); // Split the string
+let getTextFrom = (searchString, keyword) => {
+    ret = searchString.split(keyword); // Split the string
     return ret[1].trim();
 }
 
 /**
  * @name getSem
  * @description This function will go though the given string and determine which semesters are within
- * @param {string} str This string holds the information we want
+ * @param {string} courseTitle This string holds the information we want
  * @returns {string} With the semesters the course is in
  */
-let getSem = (str) => {
+let getSem = (courseTitle) => {
 
     let ret = "";
 
-    if (str.includes("Summer")) { // Checks for Summer
+    if (courseTitle.includes("Summer")) { // Checks for Summer
         ret += "S";
     }
-    if (str.includes("Fall")) { // Checks for Fall
+    if (courseTitle.includes("Fall")) { // Checks for Fall
         ret += "F";
     }
-    if (str.includes("Winter")) { // Checks for Winter
+    if (courseTitle.includes("Winter")) { // Checks for Winter
         ret += "W";
     }
-    if (str.includes("Unspecified")) { // Checks for Unspecified
+    if (courseTitle.includes("Unspecified")) { // Checks for Unspecified
         ret += "U";
     }
 
@@ -63,7 +63,7 @@ let getSem = (str) => {
 let getJSON = (inTxt) => {
 
     let j;
-    let objPage = [];
+    let courseObjectArr = [];
     let str = inTxt.split('\n'); //Splits the text so it will go line by line
 
     for (let i = 0; i < str.length; i++) { // for loop to get each line
@@ -72,17 +72,17 @@ let getJSON = (inTxt) => {
             /* ----------------- Declare Variables ----------------- */
             let tmp1 = str[i].trim(); //Get full course tittle
             let tmp = tmp1.split(/\s{2}/);
-            let name = tmp[1];
-            let cCode = str[i].substring(0, 9).trim(); //Get the course code
-            let cCred = str[i].substring(str[i].length - 7, str[i].length - 3).trim(); //Get the course credit
-            let sSem = getSem(str[i]); // Runs through the get semester function
-            let dDes = '';
-            let pPre = '';
-            let eEqu = '';
-            let oOff = '';
-            let rRes = '';
-            let dDep = '';
-            let lLoc = '';
+            let courseName = tmp[1];
+            let courseCode = str[i].substring(0, 9).trim(); //Get the course code
+            let courseCredit = str[i].substring(str[i].length - 7, str[i].length - 3).trim(); //Get the course credit
+            let courseSemester = getSem(str[i]); // Runs through the get semester function
+            let courseDescription = '';
+            let coursePrerequisite = '';
+            let courseEquate = '';
+            let courseOffering = '';
+            let courseRestriction = '';
+            let courseDepartment = '';
+            let courseLocation = '';
             let temp = 0;
             let fLoc = 0;
 
@@ -101,62 +101,62 @@ let getJSON = (inTxt) => {
                         j++
                     }
                     if (str[j].includes("Offering(s):")) { // Grabs offerings
-                        oOff = getTextFrom(str[j], "Offering(s):");
+                        courseOffering = getTextFrom(str[j], "Offering(s):");
                     } else if (str[j].includes("Restriction(s):")) { // Grabs restrictions
-                        rRes = getTextFrom(str[j], "Restriction(s):");
+                        courseRestriction = getTextFrom(str[j], "Restriction(s):");
                     } else if (str[j].includes("Equate(s):")) { // Grabs Equates
-                        eEqu = getTextFrom(str[j], "Equate(s):");
+                        courseEquate = getTextFrom(str[j], "Equate(s):");
                     } else if (str[j].includes("Department(s):")) { // Grabs departments
-                        dDep = getTextFrom(str[j], "Department(s):");
+                        courseDepartment = getTextFrom(str[j], "Department(s):");
                     } else if (str[j].includes("Location(s):") && fLoc == 0) { // Grabs locations
-                        lLoc = getTextFrom(str[j], "Location(s):");
+                        courseLocation = getTextFrom(str[j], "Location(s):");
                         fLoc == 1;
                     } else if (str[j].includes("Prerequisite(s):")) { // Grabs prerequisites
-                        pPre = getTextFrom(str[j], "Prerequisite(s):");
+                        coursePrerequisite = getTextFrom(str[j], "Prerequisite(s):");
                     } else { // Grabs descriptions
-                        dDes = str[j].trim();
+                        courseDescription = str[j].trim();
                     }
                     j++; // Increment
                 }
 
                 /* ----------------- Create the object to hold the course information ----------------- */
-                let obj = {
-                    name: name,
-                    cCode: cCode,
-                    cCred: cCred,
-                    sSem: sSem,
-                    dDes: dDes,
-                    pPre: pPre,
-                    oOff: oOff,
-                    eEqu: eEqu,
-                    rRes: rRes,
-                    dDep: dDep,
-                    lLoc: lLoc
+                let courseObject = {
+                    courseName: courseName,
+                    courseCode: courseCode,
+                    courseCredit: courseCredit,
+                    courseSemester: courseSemester,
+                    courseDescription: courseDescription,
+                    coursePrerequisite: coursePrerequisite,
+                    courseOffering: courseOffering,
+                    courseEquate: courseEquate,
+                    courseRestriction: courseRestriction,
+                    courseDepartment: courseDepartment,
+                    courseLocation: courseLocation
                 };
 
                 /* ----------------- Add each course to the appropriate semester array ----------------- */
-                if ((obj.sSem).includes("W")) {
-                    wWin.push(obj);
+                if ((courseObject.courseSemester).includes("W")) {
+                    winterCoursesArr.push(courseObject);
                 }
-                if ((obj.sSem).includes("S")) {
-                    sSum.push(obj);
+                if ((courseObject.courseSemester).includes("S")) {
+                    summerCoursesArr.push(courseObject);
                 }
-                if ((obj.sSem).includes("F")) {
-                    fFal.push(obj);
+                if ((courseObject.courseSemester).includes("F")) {
+                    fallCoursesArr.push(courseObject);
                 }
-                objPage.push(obj); // Add that object to the array of objects
+                courseObjectArr.push(courseObject); // Add that object to the array of objects
             }
         }
     }
-    return objPage;
+    return courseObjectArr;
 }
 
 /**
  * @name getJSONFile
  * @description This fucntion will print all the courses for json files
- * @param {Object array} aProg is all the programs
+ * @param {Object array} programArr is all the programs
  */
-let getJSONFile = (aProg) => {
+let getJSONFile = (programArr) => {
     if (!fs.existsSync("./json")) { // Checks to see if the folder exists
         fs.mkdir("./json", (err) => {
             if (err) {
@@ -164,22 +164,22 @@ let getJSONFile = (aProg) => {
             }
         });
     }
-    fs.writeFile('./json/AllCourses.json', JSON.stringify(aProg, null, '\t'), (err) => {
+    fs.writeFile('./json/AllCourses.json', JSON.stringify(programArr, null, '\t'), (err) => {
         if (err) {
             throw err;
         }
     });
-    fs.writeFile('./json/Winter.json', JSON.stringify(wWin, null, '\t'), (err) => {
+    fs.writeFile('./json/Winter.json', JSON.stringify(winterCoursesArr, null, '\t'), (err) => {
         if (err) {
             throw err;
         }
     });
-    fs.writeFile('./json/Fall.json', JSON.stringify(fFal, null, '\t'), (err) => {
+    fs.writeFile('./json/Fall.json', JSON.stringify(fallCoursesArr, null, '\t'), (err) => {
         if (err) {
             throw err;
         }
     });
-    fs.writeFile('./json/Summer.json', JSON.stringify(sSum, null, '\t'), (err) => {
+    fs.writeFile('./json/Summer.json', JSON.stringify(summerCoursesArr, null, '\t'), (err) => {
         if (err) {
             throw err;
         }
@@ -213,7 +213,7 @@ async function main() {
 
     // console.log(programCodes.length);
 
-    let aProg = [];
+    let programArr = [];
 
 
     // Tell the tab to navigate to the various program topic pages.
@@ -228,34 +228,34 @@ async function main() {
         tPtr = title.split('(');
 
         // Get the program name and code
-        let pName = tPtr[0].trim();
-        let pCode;
+        let programName = tPtr[0].trim();
+        let programCode;
 
         // Grab the program code from the title
         if (tPtr[1] != null && (tPtr[1].length == 5)) {
-            pCode = tPtr[1].substring(0, 4);
+            programCode = tPtr[1].substring(0, 4);
         } else if (tPtr[1] != null && (tPtr[1].length == 4)) {
-            pCode = tPtr[1].substring(0, 3);
+            programCode = tPtr[1].substring(0, 3);
         } else if (tPtr[1] != null && (tPtr[1].length == 3)) {
-            pCode = tPtr[1].substring(0, 2);
+            programCode = tPtr[1].substring(0, 2);
         }
 
         // Get all the text within the program page
         let inTxt = await page.innerText('div.sc_sccoursedescs'); // Grabs a string from that page
 
         // Program object
-        let obj = {
-            pName: pName,
-            pCode: pCode,
-            pCour: getJSON(inTxt) // Get the object array from that string
+        let programObject = {
+            programName: programName,
+            programCode: programCode,
+            programCourse: getJSON(inTxt) // Get the object array from that string
         };
 
         // Add to the overall 
-        aProg.push(obj);
+        programArr.push(programObject);
     }
 
     // Print the courses to a JSON folder
-    getJSONFile(aProg);
+    getJSONFile(programArr);
 
     // Turn off the browser to clean up after ourselves.
     await browser.close();
