af409e5581214cb5ee9f1711fe33ff63aefc39d6
Harsh Topiwala <htopiwal@uoguelph.ca>
2022-01-30 22:18:24 -0500

    GRAPH: Added graph visualization functionality

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -160,21 +160,30 @@ let getJSON = (inTxt) => {
             /* ----------------- Declare Variables ----------------- */
             let tmp1 = str[i].trim(); //Get full course tittle
             let tmp = tmp1.split(/\s{2}/);
-            let courseName = tmp[1];
-            let courseCode = str[i].substring(0, 9).trim(); //Get the course code
-            let courseCredit = str[i].substring(str[i].length - 7, str[i].length - 3).trim(); //Get the course credit
-            let courseSemester = getSem(str[i]); // Runs through the get semester function
-            let courseDescription = '';
-            let coursePrerequisite = '';
-            let courseEquate = '';
-            let courseOffering = '';
-            let courseRestriction = '';
-            let courseDepartment = '';
-            let courseLocation = '';
-            let coursePrerequisiteCodes = [];
             let temp = 0;
             let fLoc = 0;
 
+            /* ----------------- Create the object to hold the course information ----------------- */
+            let prerequisiteObject = {
+                or_courses: [],
+                mandatory: []
+            }
+            
+            let courseObject = {
+                name: tmp[1],
+                code: str[i].substring(0, 9).trim(),
+                credit: str[i].substring(str[i].length - 7, str[i].length - 3).trim(),
+                semester: getSem(str[i]),
+                description: '',
+                prerequisites: '',
+                offering: '',
+                equate: '',
+                restriction: '',
+                department: '',
+                location: '',
+                prerequisiteCodes: prerequisiteObject
+            };
+            
             if (str[i].charAt(2) == '*' || str[i].charAt(3) == '*' || str[i].charAt(4) == "*") { // This makes sure to grab only the title
 
 
@@ -190,41 +199,25 @@ let getJSON = (inTxt) => {
                         j++
                     }
                     if (str[j].includes("Offering(s):")) { // Grabs offerings
-                        courseOffering = getTextFrom(str[j], "Offering(s):");
+                        courseObject.offering = getTextFrom(str[j], "Offering(s):");
                     } else if (str[j].includes("Restriction(s):")) { // Grabs restrictions
-                        courseRestriction = getTextFrom(str[j], "Restriction(s):");
+                        courseObject.restriction = getTextFrom(str[j], "Restriction(s):");
                     } else if (str[j].includes("Equate(s):")) { // Grabs Equates
-                        courseEquate = getTextFrom(str[j], "Equate(s):");
+                        courseObject.equate = getTextFrom(str[j], "Equate(s):");
                     } else if (str[j].includes("Department(s):")) { // Grabs departments
-                        courseDepartment = getTextFrom(str[j], "Department(s):");
+                        courseObject.department = getTextFrom(str[j], "Department(s):");
                     } else if (str[j].includes("Location(s):") && fLoc == 0) { // Grabs locations
-                        courseLocation = getTextFrom(str[j], "Location(s):");
+                        courseObject.location = getTextFrom(str[j], "Location(s):");
                         fLoc == 1;
                     } else if (str[j].includes("Prerequisite(s):")) { // Grabs prerequisites
-                        coursePrerequisite = getTextFrom(str[j], "Prerequisite(s):");
-                        coursePrerequisiteCodes = getPreCode(coursePrerequisite);
+                        courseObject.prerequisites = getTextFrom(str[j], "Prerequisite(s):");
+                        courseObject.prerequisiteCodes = getPreCode(courseObject.prerequisites);
                     } else { // Grabs descriptions
-                        courseDescription = str[j].trim();
+                        courseObject.description = str[j].trim();
                     }
                     j++; // Increment
                 }
-
-                /* ----------------- Create the object to hold the course information ----------------- */
-                let courseObject = {
-                    name: courseName,
-                    code: courseCode,
-                    credit: courseCredit,
-                    semester: courseSemester,
-                    description: courseDescription,
-                    prerequisites: coursePrerequisite,
-                    offering: courseOffering,
-                    equate: courseEquate,
-                    restriction: courseRestriction,
-                    department: courseDepartment,
-                    location: courseLocation,
-                    prerequisiteCodes: coursePrerequisiteCodes
-                };
-
+                
                 /* ----------------- Add each course to the appropriate semester array ----------------- */
                 if ((courseObject.semester).includes("W")) {
                     winterCoursesArray.push(courseObject);
@@ -237,6 +230,7 @@ let getJSON = (inTxt) => {
                 }
                 courseObjectArr.push(courseObject); // Add that object to the array of objects
             }
+            
         }
     }
     return courseObjectArr;
--- search/courseSearch.py
+++ search/courseSearch.py
@@ -27,31 +27,31 @@ def parseCLIArgs():
 
 
     # Parser initialization
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog='tool', formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=10))
     subparsers = parser.add_subparsers(help='Search Methods')
 
     # Case 1: list all programs
     #   courseSearch lp
-    listProgramsParser = subparsers.add_parser('lp')
+    listProgramsParser = subparsers.add_parser('lp', help='List all programs and their codes, i.e python3 courseSearch.py lp')
     listProgramsParser.set_defaults(which='lp')
 
     
     # Case 2: search by course code: 
     #   courseSearch cc {Course code, i.e: ACCT*1000}
-    ccParser = subparsers.add_parser('cc', help='Search by course code')
+    ccParser = subparsers.add_parser('cc', help='Search by course code, i.e python3 courseSearch.py cc {course code, i.e ACCT*1220}')
     ccParser.add_argument('[Course Code]')
     ccParser.set_defaults(which='cc')
 
     # Case 3: search by program code: 
     #   courseSearch pc {Program code}
-    pcParser = subparsers.add_parser('pc', help='Search by program code')
+    pcParser = subparsers.add_parser('pc', help='Search by program code, i.e python3 courseSearch.py pc {program code, i.e ACCT}')
     pcParser.add_argument('[Program Code]')
     pcParser.set_defaults(which='pc')
 
 
     # Case 4: search by credit weight and season: 
     #   courseSearch cw {0.25, 0.5, 0.75, ... } {Season: optional, default all seasons}
-    cwParser = subparsers.add_parser('cw', help='Search by course weight and season (optional)')
+    cwParser = subparsers.add_parser('cw', help='Search by course weight and season (optional), i.e python3 courseSearch.py cw {course weight, i.e 0.25}, {season (optional), i.e S}')
     cwParser.add_argument('[Course Weight]', choices=['0.00', '0.25', '0.50', '0.75', '1.00'])
     cwParser.add_argument('[Offering Season]', type=str.upper, nargs='?', default='all', choices=['S', 'W', 'F', 'ALL'])
     cwParser.set_defaults(which='cw')
